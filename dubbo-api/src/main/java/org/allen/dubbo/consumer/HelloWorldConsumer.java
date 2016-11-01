99c16ddd0a602fe5f9e10f3da66b03e194127cee
fuyong <fuyong@niwodai.net>
2016-11-01 17:27:13 +0800

    use java reflect to reference which service to call

--- dubbo-api/src/main/java/org/allen/dubbo/consumer/HelloWorldConsumer.java
+++ dubbo-api/src/main/java/org/allen/dubbo/consumer/HelloWorldConsumer.java
@@ -5,9 +5,25 @@ import com.alibaba.dubbo.config.ReferenceConfig;
 import com.alibaba.dubbo.config.RegistryConfig;
 import org.allen.dubbo.service.HelloWorldService;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class HelloWorldConsumer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        reflect();
+
+        System.out.println("*** press any key to quit ****");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("*** bye! bye! ****");
+    }
+
+    public static void normal() {
         ApplicationConfig applicationConfig = new ApplicationConfig("hello-consumer");
 
         RegistryConfig registryConfig = new RegistryConfig("zookeeper://172.16.23.68:2184");
@@ -20,5 +36,27 @@ public class HelloWorldConsumer {
 
         HelloWorldService helloWorldService = referenceConfig.get();
         System.out.println(helloWorldService.hello("dubbo-api"));
+    }
+
+    public static void reflect() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        String className = "org.allen.dubbo.service.HelloWorldService";
+        String methodName = "hello";
+
+        ApplicationConfig applicationConfig = new ApplicationConfig("hello-consumer");
+
+        RegistryConfig registryConfig = new RegistryConfig("zookeeper://172.16.23.68:2184");
+
+        Class clazz = Class.forName(className);
+
+        ReferenceConfig referenceConfig = new ReferenceConfig();
+        referenceConfig.setApplication(applicationConfig);
+        referenceConfig.setRegistry(registryConfig);
+        referenceConfig.setInterface(clazz);
+        referenceConfig.setVersion("1.0.0");
+
+        Object object = referenceConfig.get();
+        Method method = clazz.getMethod(methodName, String.class);
+        Object result = method.invoke(object, "dubbo-api");
+        System.out.println(result);
     }
 }
--- dubbo-api/src/main/java/org/allen/dubbo/service/HelloWorldServiceImpl.java
+++ dubbo-api/src/main/java/org/allen/dubbo/service/HelloWorldServiceImpl.java
@@ -3,6 +3,7 @@ package org.allen.dubbo.service;
 public class HelloWorldServiceImpl implements HelloWorldService {
     @Override
     public String hello(String msg) {
+        System.out.println(String.format("receive dubbo call, msg: %s", msg));
         return "hello " + msg;
     }
 }
